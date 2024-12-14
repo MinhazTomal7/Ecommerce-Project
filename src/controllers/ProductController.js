60e9994d6b81e4a1b926361a6040cd767b275b21
Minhaz Tomal <aminminhaz55@gmail.com>
2024-12-14 21:40:51 +0600

    upto ListByBrandService

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -17,23 +17,37 @@ const {
 
 exports.ProductBrandList = async(req, res)=>{
 
-
+    let result = await BrandListService()
+    return res.status(200).json(result)
 }
+
 exports.ProductCategoryList = async(req, res)=>{
 
-
+    let result = await CategoryListService()
+    return res.status(200).json(result)
 }
 
 exports.ProductSliderList = async(req, res)=>{
 
+    let result = await SliderListService()
+    return res.status(200).json(result)
 }
 
-exports.ProductListByBrand = async(req, res)=>{
 
-}
+exports.ProductListByBrand = async (req, res) => {
+    try {
+        const BrandID = req.params.BrandID;
+        let result = await ListByBrandService(BrandID);
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json({ status: "Failed", data: err.toString() });
+    }
+};
+
 
 exports.ProductListByCategory= async(req, res)=>{
-
+    let result = await ListByCategoryService()
+    return res.status(200).json(result)
 }
 
 exports.ProductListBySmiler= async(req, res)=> {
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,29 +1,88 @@
-
+const mongoose = require("mongoose");
 const BrandModel = require('../models/BrandModel');
 const CategoryModel = require('../models/CategoryModel');
 const ProductSliderModel = require('../models/ProductSliderModel');
 const ProductModel = require('../models/ProductModel');
 const ProductDetailModel = require('../models/ProductDetailModel');
 const ReviewModel = require('../models/ReviewModel');
+const ObjectID = mongoose.Types.ObjectId;
 
 
 
 
 const BrandListService = async ()=>{
+    try{
+       let data = await BrandModel.find()
+        return{status:"Success", data:data}
+
+    }
+    catch (err){
+        return{status:"Failed", data:err}.toString()
+    }
 
 }
 
 const CategoryListService = async ()=>{
+    try{
+        let data = await CategoryModel.find()
+        return{status:"Success", data:data}
+
+    }
+    catch (err){
+        return{status:"Failed", data:err}.toString()
+    }
 
 }
 
 const SliderListService = async ()=>{
+    try{
+        let data = await ProductSliderModel.find()
+        return{status:"Success", data:data}
+
+    }
+    catch (err){
+        return{status:"Failed", data:err}.toString()
+    }
 
 }
 
-const ListByBrandService = async ()=>{
+const ListByBrandService = async (BrandID) => {
+    try {
+        let MatchStage = { $match: { brandID: new ObjectID(BrandID) } };
+        let JoinWithBrandStage = {
+            $lookup: {
+                from: "brands",
+                localField: "brandID",
+                foreignField: "_id",
+                as: "brands",
+            },
+        };
+        let JoinWithCategoryStage = {
+            $lookup: {
+                from: "categories",
+                localField: "categoryID",
+                foreignField: "_id",
+                as: "categories",
+            },
+        };
 
-}
+        let UnwindBrandStage = {$unwind:"$brand"}
+        let UnwindCategoryStage = {$unwind:"$category"}
+        let ProjectionStage={$project:{'brand._id':0, 'category._id':0, 'brandID':0, 'categoryID':0}}
+
+        let data = await ProductModel.aggregate([
+            MatchStage,
+            JoinWithBrandStage,
+            JoinWithCategoryStage,
+            UnwindBrandStage, UnwindCategoryStage, ProjectionStage
+        ]);
+
+        return { status: "Success", data: data };
+    } catch (err) {
+        return { status: "Failed", data: err.toString() };
+    }
+};
+
 
 const ListByCategoryService = async ()=>{
 
